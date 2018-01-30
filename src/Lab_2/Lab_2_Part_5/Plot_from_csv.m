7cdc99feed9997aa82d29cdc1318c2d44ca4b0c2
embarinelli <embarinelli@wpi.edu>
2018-01-29 20:47:33 -0500

    finished part 5

--- src/Lab_2/Lab_2_Part_5/Plot_from_csv.m
+++ src/Lab_2/Lab_2_Part_5/Plot_from_csv.m
@@ -12,17 +12,17 @@ fig_size = get(0, 'Screensize');
 fig_pos = [0,0,... %fig_size(3), fig_size(4), ...
 0.9*fig_size(3), 0.8*fig_size(4)];
 set(f, 'Position', fig_pos);
-axis((Robot.l2 + Robot.l3) * [-1 1 -1 1 -0.5 1.5]);
+% axis((Robot.l2 + Robot.l3) * [-1 1 -1 1 -0.5 1.5]);
 title('End Effector Path');
 xlabel('X Axis [m]'); ylabel('Y Axis [m]'); zlabel('Z Axis [m]');
 
 % Columns 1, 5, 9, 13, 17, (n*4)-3 are the timestamp, n=1:5
 % Columns 2, 6, 10, 14, 18 (n*4)-2 are the Xs. n = 1:5
 
-% Keeps track of the row.
+% This is the row we are on.
 i = 1;
 
-% Read in the csv file.
+% This is the giant csv file read in.
 M = dlmread('joints.csv');
 
 % X Coordinates
@@ -33,11 +33,6 @@ Ys = [];
 
 % Z Coordinates
 Zs = [];
-
-% We have a 10x4 for each translation/points
-% n = x, y or z components
-% m = 
-% l =
 
 % The reformatted data from the .csv file.
 importedFromCSV = [];
