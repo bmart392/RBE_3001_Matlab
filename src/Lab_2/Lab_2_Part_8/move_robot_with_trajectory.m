248e3166fd7647791020102c4d1f5e6de1c48520
bmart392 <bmart392@gmail.com>
2018-01-30 16:14:06 -0500

    code that was signed off for for part 8, needs some cleaning up and organization

--- src/Lab_2/Lab_2_Part_8/move_robot_with_trajectory.m
+++ src/Lab_2/Lab_2_Part_8/move_robot_with_trajectory.m
@@ -11,17 +11,17 @@ import java.lang.*;
 clc; clear; close all;
 
 % Create a PacketProcessor object to send data to the nucleo firmware
-pp = PacketProcessor(7); 
+pp = PacketProcessor(7);
 
 % try
 
 % Creating the robot structure.
 Robot.l1 = 0.135;
-Robot.l2 = 0.175; 
+Robot.l2 = 0.175;
 Robot.l3  = 0.16928;
 
 % y0 is the initial conditions.
-y0 = kinematics([0; 0; 0]);           
+y0 = kinematics([0; 0; 0]);
 
 %% Plotting code.
 
@@ -35,16 +35,16 @@ grid on;
 % center the figure on screen and resize it
 fig_size = get(0, 'Screensize');
 fig_pos = [0,0,... %fig_size(3), fig_size(4), ...
-0.9*fig_size(3), 0.8*fig_size(4)];
+    0.9*fig_size(3), 0.8*fig_size(4)];
 set(f, 'Position', fig_pos);
 axis((Robot.l2 + Robot.l3) * [-1 1 -1 1 -0.5 1.5]);
 title('Stick figure plot');
 xlabel('X Axis [m]'); ylabel('Y Axis [m]'); zlabel('Z Axis [m]');
 
 % Setting the "handle" field in "Robot" structure to be the handle of
-% the arm plot. 
+% the arm plot.
 Robot.handle = plot3(y0(:,1),y0(:,2),y0(:,3),'-o', ...
-'color', [0 0.4 0.7], 'LineWidth', 5);
+    'color', [0 0.4 0.7], 'LineWidth', 5);
 
 
 PIDID  = 37;            % This controls the robot
@@ -60,7 +60,7 @@ pidpacket= zeros(15, 1, 'single');
 
 % The following code generates a sinusoidal trajectory to be
 % executed on joint 1 of the arm and iteratively sends the list of
-% setpoints to the Nucleo firmware. 
+% setpoints to the Nucleo firmware.
 % viaPts = [0, -400, 400, -400, 400, 0];
 
 % 11.44 ticks per degree
@@ -69,58 +69,64 @@ angconv = 11.44;
 j = 1;               % joint 1 = base, 2 = elbow, 3 = wrist
 jointmatrix = [10,2];  % matrix to hold joint angles and time
 i = 1;               % row of matrix
-time = 0;            % make sure time starts at 0  
+time = 0;            % make sure time starts at 0
 
-endeffectLocation = zeros(5,3);
+%endeffectLocation = zeros(5,3);
 
-theseones = [1 4 7];
+%theseones = [1 4 7];
 
 readings = zeros(3,1);
 
-pos1 = [ 0 5 5 ];
-numinterpolatoins = 10;
+pos = [ 0 26.0 -150.5; 0 -41.25 476.5; ...
+    0 756.0 -129.75; ];
+numinterpolations = 10;
 
 
-pos_1_linkcoeff_1 = [0 5, 0, 0, 0, pos1(1)];
-pos_1_linkcoeff_2 = [0 5, 0, 0, 0, pos1(2)];
-pos_1_linkcoeff_3 = [0 5, 0, 0, 0, pos1(3)];
-desiredpos(:,:) = gentraj_fullrobot(pos_1_linkcoeff_1,... 
-    pos_1_linkcoeff_2,pos_1_linkcoeff_3,numinterpolations);
 
-for k = 0:3
-% send the packet with data for the mth location
-returnpidpacket = pp.command(PIDID, pidpacket);
-
-%-track the location of the arm getting to the mth location --
-% track the status of the arm while moving to each position
-for i = 1:10
-% save time
-time = toc;
-% check status
-returnstatuspacket = pp.command(STATUSID, statuspacket);
-readings(1,1) = returnstatuspacket(1);
-readings(2,1) = returnstatuspacket(4);
-readings(3,1) = returnstatuspacket(7);
-
-% Plot the robot transformations given.
-RobotPlotter(Robot, readings);
-drawnow;
-
-answer = kinematics(readings);
-% put in matrix
-jointmatrix(i, (4*m)-3) = time;
-jointmatrix(i, (4*m)-2) = answer(4,1);
-jointmatrix(i, (4*m)-1) = answer(4,2);
-jointmatrix(i, 4*m) = answer(4,3);
-end
-
-pause(2);
-
+desiredpos(:,:) = threepointtraj(pos(1,:),...
+    pos(2,:),pos(3,:),numinterpolations);
+disp(desiredpos);
+tic
+for k = 1:30
+    for u = 0:2
+        pidpacket((3*u)+1) = desiredpos(k,u+1);
+    end
+    
+    % send the packet with data for the mth location
+    returnpidpacket = pp.command(PIDID, pidpacket);
+    
+    %-track the location of the arm getting to the mth location --
+    % track the status of the arm while moving to each position
+    
+        for i = 1:2
+            % save time
+            time = toc;
+            % check status
+            returnstatuspacket = pp.command(STATUSID, statuspacket);
+            readings(1,1) = returnstatuspacket(1);
+            readings(2,1) = returnstatuspacket(4);
+            readings(3,1) = returnstatuspacket(7);
+            
+            % Plot the robot transformations given.
+            %RobotPlotter(Robot, readings);
+            %drawnow;
+            
+            %answer = kinematics(readings);
+            % put in matrix
+            jointmatrix(((2*(k-1))+i), 1) = time;
+            jointmatrix(((2*(k-1))+i), 2) = readings(1,1);
+            jointmatrix(((2*(k-1))+i), 3) = readings(2,1);
+            jointmatrix(((2*(k-1))+i), 4) = readings(3,1);
+        end
+    
+    
+    %pause(1);
+    
 end
 
 % move back to home position
 for d=0:2
-pidpacket((d*3)+1) = 1;
+    pidpacket((d*3)+1) = 1;
 end
 
 returnpidpacket = pp.command(PIDID, pidpacket);
@@ -128,7 +134,7 @@ returnpidpacket = pp.command(PIDID, pidpacket);
 pause(2);
 
 % save to csv
-csvwrite('joints.csv', jointmatrix);
+csvwrite('interpolationjoints.csv', jointmatrix);
 
 % catch
 % disp('Exited on error, clean shutdown');
---
+++ src/Lab_2/Lab_2_Part_8/plot_interpolated_triangle.m
@@ -0,0 +1,104 @@
+
+f = figure; % create figure
+axes;
+hold on;
+%axis equal;
+box on;
+grid on;
+
+% center the figure on screen and resize it
+fig_size = get(0, 'Screensize');
+fig_pos = [0,0,0.9*fig_size(3), 0.8*fig_size(4)];
+set(f, 'Position', fig_pos);
+axis([0 18 -20 80]);
+title('Plotting triangle path');
+xlabel('Time [s]'); ylabel('Position [degrees]');
+
+% This is the giant csv file read in.
+M = dlmread('interpolationjoints.csv');
+
+% X Coordinates
+Xs = [];
+
+% Y Coordinates
+Ys = [];
+
+% The reformatted data from the .csv file.
+importedFromCSV = zeros(60,4);
+
+% Counter for counting the rows in the l for loop
+i = 1;
+%iterate through each axis
+for m=1:4
+    % iterate through the columns for the data from each axis
+    
+    
+    % iterate through the rows of the csv
+    for l=1:60
+        importedFromCSV(l,m) = M(l,m);
+        
+    end
+    
+    
+    
+end
+
+% Fill in the respective column vectors.
+J1s(:,1) = importedFromCSV(:,1);
+J1s(:,2) = importedFromCSV(:,2)./11.4;
+
+vJ1s = importedFromCSV(:,1);
+for k = 1:29
+    vJ1s(k,2) = (J1s((k+1),2)-J1s(k,2))/(J1s(k+1,1)-J1s(k,1));
+end
+
+
+
+J2s(:,1) = importedFromCSV(:,1);
+J2s(:,2) = importedFromCSV(:,3)./11.4;
+
+vJ2s = importedFromCSV(:,1);
+for w = 1:29
+    vJ2s(w,2) = (J2s((w+1),2)-J2s(w,2))/(J2s(w+1,1)-J2s(w,1));
+    
+end
+
+
+J3s(:,1) = importedFromCSV(:,1);
+J3s(:,2) = importedFromCSV(:,4)./11.4;
+
+vJ3s = importedFromCSV(:,1);
+for p = 1:29
+    vJ3s(p,2) = (J3s((p+1),2)-J3s(p,2))/(J3s(p+1,1)-J3s(p,1));
+end
+
+
+times = importedFromCSV(:,1);
+
+
+
+plot(J1s(:,1),J1s(:,2),J2s(:,1),J2s(:,2),...
+    J3s(:,1),J3s(:,2));
+legend('Joint 1 Position','Joint 2 Position','Joint 3 Position')
+
+disp(vJ1s);
+
+
+g = figure; % create figure
+axes;
+hold on;
+%axis equal;
+box on;
+grid on;
+
+% center the figure on screen and resize it
+fig_size = get(0, 'Screensize');
+fig_pos = [0,0,0.9*fig_size(3), 0.8*fig_size(4)];
+set(g, 'Position', fig_pos);
+axis([0 9 -25 25]);
+title('Plotting triangle path velocity');
+xlabel('Time [s]'); ylabel('Velocity [degrees/s]');
+plot(vJ1s(:,1),vJ1s(:,2),vJ2s(:,1),vJ2s(:,2), ...
+    vJ3s(:,1),vJ3s(:,2));
+
+legend('Joint 1 Velocity','Joint 2 Velocity','Joint 3 Velocity')
