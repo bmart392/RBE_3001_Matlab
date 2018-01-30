dc40ef4745ba6aad83155834b6b8242350d5bb4e
embarinelli <embarinelli@wpi.edu>
2018-01-29 20:19:26 -0500

    changed name of plot

--- src/Lab_2/Lab_2_Part_5/Plot_from_csv.m
+++ src/Lab_2/Lab_2_Part_5/Plot_from_csv.m
@@ -13,22 +13,24 @@ fig_pos = [0,0,... %fig_size(3), fig_size(4), ...
 0.9*fig_size(3), 0.8*fig_size(4)];
 set(f, 'Position', fig_pos);
 axis((Robot.l2 + Robot.l3) * [-1 1 -1 1 -0.5 1.5]);
-title('Stick figure plot');
+title('End Effector Path');
 xlabel('X Axis [m]'); ylabel('Y Axis [m]'); zlabel('Z Axis [m]');
 
 % Columns 1, 5, 9, 13, 17, (n*4)-3 are the timestamp, n=1:5
 % Columns 2, 6, 10, 14, 18 (n*4)-2 are the Xs. n = 1:5
 
-% This is the row we are on.
+% Keeps track of the row.
 i = 1;
 
-% This is the giant csv file read in.
+% Read in the csv file.
 M = dlmread('joints.csv');
 
 importedFromCSV = [];
 
 % We have a 10x4 for each translation/points
-
+% n = x, y or z components
+% m = 
+% l =
 for n=1:3
     for m=1:5
         for l=1:10
