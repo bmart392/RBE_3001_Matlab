2ca253f0a29ab85a7dc3b51d4666d41a88ef53c4
Benjamin Mart <bmart392@gmail.com>
2018-02-02 12:33:25 -0500

    Created more dialog box functions

---
+++ src/Lab_3/Utility_Function/dialog_box_3option.m
@@ -0,0 +1,26 @@
+% Function displays a dialog box that allows the user to input data into
+% the script
+% INPUTS:   question = a character array of the message the user should see
+%           title = a character array of the title of the dialog box
+%           option1 = option that appears on the left most button
+%           option2 = option that appears on the center button
+%           option3 = option that appears on the right most button
+% OUTPUT:   a number representing the choice of the user
+%           NOTE: the number returned will correspond to the order of the
+%           options
+function input = dialog_box_3option(question,title,option1,...
+    option2,option3)
+% Construct a questdlg with three options
+choice = questdlg(question, title,option1,option2,option3,option3);
+
+% Handle response
+switch choice
+    case option1
+        input = 1;
+    case option2
+        input = 2;
+    case option3
+        input = 3;
+        
+end
+end
