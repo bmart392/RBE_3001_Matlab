db50fa85e20966c45dc4497243faf6be5d63e9fa
thatrobotguy <andrewtheschueler@gmail.com>
2018-02-07 15:53:44 -0500

    Just talked with TAs and fixed my little friend called Jacob.

--- src/Lab_4/Lab_4_Step2/jacob0.m
+++ src/Lab_4/Lab_4_Step2/jacob0.m
@@ -14,17 +14,18 @@ theta1 = q(1,1)*degreesToRadians;
 theta2 = q(2,1)*degreesToRadians;
 theta3 = q(3,1)*degreesToRadians;
 
+        % This half above is the linear velocity angle contributions
 j = [ (-1*((sin(theta1))*L3*(cos(theta2+theta3))+L2*(sin(theta1))))      (-1*(cos(theta1))*L3*(sin(theta2 + theta3)))      (-1*(cos(theta1))*L3*(sin(theta2 + theta3)));
 
            ((cos(theta1))*L3*(cos(theta2+theta3))+L2*(cos(theta1)))         (-1*(sin(theta1))*L3*(sin(theta2 + theta3)))      (-1*(sin(theta1))*L3*(sin(theta2 + theta3)));
 
                                     0                                               (-1*L3*(cos(theta2 + theta3)))                    (-1*L3*(cos(theta2 + theta3)));
+        % This half below is the rotational velocity angle ontributions
+                               cos(theta1)                                                    cos(theta1)                                       cos(theta1);
+
+                              -sin(theta1)                                                   -sin(theta1)                                      -sin(theta1);
 
                                     0                                                             0                                                  0
-
-                                    0                                                             0                                                  0
-
-                                    1                                                             1                                                  1
             ];
  
  end
