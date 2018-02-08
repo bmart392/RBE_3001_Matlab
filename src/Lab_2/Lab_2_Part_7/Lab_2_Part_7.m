a03f11e3f60c5e8f06f212ca68844b615f117a42
Benjamin Mart <bmart392@gmail.com>
2018-02-08 10:46:52 -0500

    Created an improved folder structure in the Utility function folder. All funcitons that pertain to a certain action are in their own folder

---
+++ src/Lab_2/Lab_2_Part_7/Lab_2_Part_7.m
@@ -0,0 +1,2 @@
+%% The files formerly found in this folder can now be
+% found in /Utility_Function/Quintic_Trajectory_Generation
---
+++ src/Lab_3/Lab_3_Part_6/Lab_3_Part_6.m
@@ -0,0 +1,2 @@
+%% The files formerly found in this folder can now be
+% found in /Utility_Function/Cubic_Trajectory_Generation
