8de0bcaea9849696d9ec28c08250f0dd38e17a32
Benjamin Mart <bmart392@gmail.com>
2018-02-09 14:43:13 -0500

    Got the quintic version of the trajectory function working. Will work on the cubic version next.

--- src/Utility_Function/Quintic_Trajectory_Generation/full_trajgen_quintic.m
+++ src/Utility_Function/Quintic_Trajectory_Generation/full_trajgen_quintic.m
@@ -3,6 +3,9 @@
 % characteristics. This function is designed to generate a path of either
 % cartesian coordinates or joint angles.
 %
+% OUTPUT NOTE: This function will generate a trajectory with points =
+% (((number of steps between points+1)*(number of vertices-1)) + 1)
+%
 % This function has two versions:
 %       VERSION 1: Assumes the starting time, start velocity, end velocity,
 %       start acceleration, and end acceleration to be 0.
@@ -18,34 +21,33 @@
 %   INPUTS: version = specifies the version of the code to use. {1 = switch
 %                           statement 1 is executed; 2 = switch statement
 %                           2 is executed}
-%           vertices = a n x m matrix that holds each vertex in a 
+%           vertices = a n x m matrix that holds each vertex in a
 %                       column. Must be a least 1 x 1.
-%           time_elapsed = a 1 x m matrix that holds the amount of time 
-%                           in which each part of the trajectory a leg is 
+%           time_elapsed = a 1 x m matrix that holds the amount of time
+%                           in which each part of the trajectory a leg is
 %                           to be executed.
 %           abbreviated_numsteps = the number of points to generate for
-%                                   each part of the trajectory 
+%                                   each part of the trajectory
 %   OUTPUT: trajectory = a n x m matrix of points that are the trajectory
-%                           of the robot. Each column of the matrix 
+%                           of the robot. Each column of the matrix
 %                           represents point
 %
 % ----- VERSION 2 -----
-%   FUNCTION CALL: 
+%   FUNCTION CALL:
 %       full_trajgen_quintic(2,0,0,0, joint1_pointcharacteristics,...
 %               joint2_pointcharacteristics,joint3_pointcharacteristics,
 %               numsteps);
-% 
+%
 % INPUTS:The characteristics of each point are defined in the
 %           gentraj_interpolation_quintic function
-%           
+%
 %           joint1_pointcharacteristics = the characteristics of joint 1
 %           joint2_pointcharacteristics = the characteristics of joint 2
 %           joint3_pointcharacteristics = the characteristics of joint 3
 %           numsteps = the number of steps interpolated in each trajectory
 %
-% OUTPUT:   a 3 x n matrix where each column holds all of the values for 
+% OUTPUT:   a 3 x n matrix where each column holds all of the values for
 %           one link and each row holds the joint values for one point
-%
 function trajectory = full_trajgen_quintic(version, vertices, ...
     time_elapsed, abbreviated_numsteps, ...
     joint1_pointcharacteristics, joint2_pointcharacteristics, ...
@@ -55,10 +57,15 @@ function trajectory = full_trajgen_quintic(version, vertices, ...
 WITHSTANDARDASSUMPTIONS = 1;    % Version 1
 NOASSUMPTIONS = 1;              % Version 2
 
+
+DONTCALCENDPOINT = 0;
+CALCENDPOINT = 1;
+
 switch version
     
     % Version 1
     case WITHSTANDARDASSUMPTIONS
+        
         
         % Define Standard Assumptions
         ASSUMED_START_TIME = 0; % seconds
@@ -86,29 +93,56 @@ switch version
             error('Incorrect number of input arguements for time_elapsed');
         end
         
-        
+        % Initialize the indeer for the final trajectory matrix
         i = 1;
         
-        trajectory = zeros(num_dimensions,(num_vertices-1)*abbreviated_numsteps);
+        % Initialize a matrix to hold the trajectory generated
+        trajectory = zeros(num_dimensions,(num_vertices-1)*...
+            abbreviated_numsteps);
         
         
-        
+        % Use a for loop to iterate through each dimension of a given point
         for j = 1:num_dimensions
+            % Use a for loop to iterate through the number of vertices in
+            % each dimension
             for k = 1:num_vertices-1
-                disp(time_elapsed(k));
-                positions = gentraj_interpolation_quintic(ASSUMED_START_TIME,...
-                    time_elapsed(k), vertices(j,k), vertices(j,k+1), ...
-                    ASSUMED_INITIAL_VELOCITY, ASSUMED_FINAL_VELOCTIY, ...
-                    ASSUMED_INITIAL_ACCELEREATION, ASSUMED_FINAL_ACCELERATION,...
-                    abbreviated_numsteps)';
-                for w = 1:abbreviated_numsteps
-                    trajectory(j,i+w-1) = positions(w);
+                % Determine if the trajectory being calculated is NOT the
+                % last one in the series of points
+                if(k ~= num_vertices-1)
+                    % Calculate the trajectory between vertices k and k+1
+                    positions = gentraj_interpolation_quintic(ASSUMED_START_TIME,...
+                        time_elapsed(k), vertices(j,k), vertices(j,k+1), ...
+                        ASSUMED_INITIAL_VELOCITY, ASSUMED_FINAL_VELOCTIY, ...
+                        ASSUMED_INITIAL_ACCELEREATION, ASSUMED_FINAL_ACCELERATION,...
+                        abbreviated_numsteps,DONTCALCENDPOINT);
+                    % Transfer those values to the return matrix
+                    for w = 1:abbreviated_numsteps+1
+                        trajectory(j,i+w-1) = positions(w);
+                    end
+                    
+                    % Increment the index for the return matrix
+                    i = i + abbreviated_numsteps +1;
+                    
                 end
                 
-                i = i + abbreviated_numsteps;
+                % Determine if the trajectory being calculated is the
+                % last one in the series of points
+                if(k == num_vertices-1)
+                    % Calculate the trajectory between vertices k and k+1
+                    positions = gentraj_interpolation_quintic(ASSUMED_START_TIME,...
+                        time_elapsed(k), vertices(j,k), vertices(j,k+1), ...
+                        ASSUMED_INITIAL_VELOCITY, ASSUMED_FINAL_VELOCTIY, ...
+                        ASSUMED_INITIAL_ACCELEREATION, ASSUMED_FINAL_ACCELERATION,...
+                        abbreviated_numsteps,CALCENDPOINT);
+                    
+                    % Transfer those values to the return matrix
+                    for w = 1:abbreviated_numsteps+2
+                        trajectory(j,i+w-1) = positions(w);
+                    end 
+                end
             end
+            % Reset the index for the return matrix
             i = 1;
-            
         end
         
         
@@ -121,7 +155,7 @@ switch version
             joint1_pointcharacteristics(2), joint1_pointcharacteristics(3), ...
             joint1_pointcharacteristics(4),joint1_pointcharacteristics(5), ...
             joint1_pointcharacteristics(6), joint1_pointcharacteristics(7), ...
-            joint1_pointcharacteristics(8), numsteps);
+            joint1_pointcharacteristics(8), numsteps, DONTCALCENDPOINT);
         
         % Calculate the trajectory of the second joint using the given
         % characteristics.
@@ -129,7 +163,7 @@ switch version
             joint2_pointcharacteristics(2), joint2_pointcharacteristics(3), ...
             joint2_pointcharacteristics(4),joint2_pointcharacteristics(5), ...
             joint2_pointcharacteristics(6), joint2_pointcharacteristics(7), ...
-            joint2_pointcharacteristics(8), numsteps);
+            joint2_pointcharacteristics(8), numsteps, DONTCALCENDPOINT);
         
         % Calculate the trajectory of the third joint using the given
         % characteristics.
@@ -137,9 +171,9 @@ switch version
             joint3_pointcharacteristics(2), joint3_pointcharacteristics(3), ...
             joint3_pointcharacteristics(4),joint3_pointcharacteristics(5), ...
             joint3_pointcharacteristics(6), joint3_pointcharacteristics(7), ...
-            joint3_pointcharacteristics(8), numsteps);
+            joint3_pointcharacteristics(8), numsteps, CALCENDPOINT);
         
-        trajectory = trajectory';
+        trajectory = trajectory;
 end
 
 end
--- src/Utility_Function/Quintic_Trajectory_Generation/gentraj_interpolation_quintic.m
+++ src/Utility_Function/Quintic_Trajectory_Generation/gentraj_interpolation_quintic.m
@@ -9,24 +9,36 @@
 %           a0 = the accelereation at position 1;
 %           af = the acceleration at position 2
 %           numpoints = the number of steps interpolated
-% OUTPUT: a 1 x 1 matrix representing the location at the given time
+%           addendpoint = a boolean value determining if an endpoint 
+%                           should be added
+% OUTPUT: a 1 X n matrix holding the trajectory of between the given points
 function points = gentraj_interpolation_quintic(t0, tf, q0, qf, v0, ...
-    vf, a0, af, numpoints)
+    vf, a0, af, numpoints, addendpoint)
+
+% Adjust the numpoints to create numpoints after the starting point.
+total_steps = numpoints+1;
 
 % Initialize a matrix to hold the points produced
-points = zeros(numpoints, 1);
+points = zeros(1, total_steps);
 
 % Create the time step and fill an array with that time step
-time = 0:(tf/(numpoints-1)):tf;
+time = 0:(tf/(total_steps)):tf;
 
 % Calculate the coefficients of the quintic polynomial equation
 coeffs = gencoeff_quintic(t0, tf, q0, qf, v0, vf, a0, af);
 
 % Calculate the position at every time value except the last value and
 % then fill the points array with those values
-for n = 1:numpoints
-    points(n,1) = genpoints_quintic(coeffs,time(n));
+for n = 1:total_steps
+    points(1,n) = genpoints_quintic(coeffs,time(n));
 end
 
+if(addendpoint == 1)
+    % Increment the n index to calculate the endpoint
+    n = n+1;
+    
+    % Calculate the end point and enter it in the matrix
+    points(1,n) = genpoints_quintic(coeffs,time(n));
+end
 end
 
