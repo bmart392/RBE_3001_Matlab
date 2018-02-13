fc050661db38699a7dba1047f95dca47a965f6ec
thatrobotguy <andrewtheschueler@gmail.com>
2018-02-12 20:24:16 -0500

    Trying to get part 7 working still

--- src/Lab_4/Lab_4_Step7/test_inverse_jacob.m
+++ src/Lab_4/Lab_4_Step7/test_inverse_jacob.m
@@ -1,5 +1,5 @@
 clc; close all; clear all;
-a = jacob0([0;0;0]); b = a(1:3,:); c = pinv(b);
+a = jacobrad([0;0;0]); b = a(1:3,:); c = pinv(b);
 disp(" This is b: "); disp(b);
 disp(" This is c: "); disp(c);
 y0 = forward_kinematics([0;0;0]);
@@ -7,13 +7,11 @@ disp(" This is y0: "); disp(y0);
 % Creating the robot structure.
 Robot.l1 = 0.135; Robot.l2 = 0.175; Robot.l3  = 0.16928;
 
-disp(" wiuerioeuwhnf ");
+disp(" final xyz location ");
 disp(y0(4,:)+[0.1 0 0.05]);
-finalForwardKinematics = y0(4,:)+[0.1 0 0.05];
-forward_offset = forward_kinematics([0;0;0]);
-currentForwardKinematics = forward_offset(4,:);
+forward_offset = y0(4,:);
+finalForwardKinematics = forward_offset + [0.1 0 0.05];
+currentForwardKinematics = forward_offset; %%%%
 
-disp((finalForwardKinematics - forward_offset(1:3,:)));
-
-deltaBeforeThresh = b*(finalForwardKinematics - forward_offset(1:3,:))';
-disp(" delta Q: "); disp(deltaBeforeThresh);
+deltaBeforeThresh = (b*(finalForwardKinematics - forward_offset)');
+disp(" delta Q: "); disp(deltaBeforeThresh.);
