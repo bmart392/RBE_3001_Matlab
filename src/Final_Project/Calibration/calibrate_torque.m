cb50dbe6af5e1c8e8d0a61343a7bf34b02756ebe
embarinelli <embarinelli@wpi.edu>
2018-02-17 16:18:39 -0500

    added torque calculation. started plotting code.

--- src/Final_Project/Calibration/calibrate_torque.m
+++ src/Final_Project/Calibration/calibrate_torque.m
@@ -14,7 +14,7 @@ clc; clear all; close all;
 
 % -------------- Communication Initialization -----------------
 
-%pp = PacketProcessor(7);    % initialize the value
+pp = PacketProcessor(7);    % initialize the value
 PID_ID = 37;                % initialize the value
 STATUS_ID = 42;             % initialize the value
 
@@ -25,46 +25,39 @@ statuspacket = zeros(16,1,'single');
 % Create a variable to hold the statuspacket
 pidpacket = zeros(15,1,'single');
 
-% -------------------------------------------------------------
+% -----------------------Set up Plot ----------------------------------
+% Creating the robot structure.
+% lengths of joints in meters
+Robot.l1 = 0.135;
+Robot.l2 = 0.175;
+Robot.l3  = 0.16928;
 
+% Set up the figure
+f = figure; % create figure
+axes;
+hold on;
+axis equal;
+box on;
+grid on;
 
-
-
-% --------------- Calibration of the offset curve -------------
-% Set variables for each possible responses
-YES = 1;
-
-user_input = dialog_box_truefalse(...
-    'Would you like to set the y0 offset for each torque sensor?'...
-    ,'Sensor Calibration');
-
-if(user_input == YES)
-    
-    % Set the position for the arm to travel
-    vertical_position = [ 0; pi/2; pi/2 ];
-    
-    % Send the arm to the vertical position
-    send_point(PID_ID,pp,pidpacket,vertical_position);
-    
-    Collect_PositionandTorque_Only = 9;    % Determine what was to be sampled
-    num_samples = 1;            % The number of samples to take
-    statuspacket(16) = 1;       % Set the status packet to tell the
-    %  nucleo to sample torque
-    
-    % Sample the the arm to read the torque sensors
-    sampled_torque = collect_n_samples(Collect_PositionandTorque_Only,num_samples,...
-        STATUS_ID,pp, statuspacket);
-    
-    % Display the sample
-    for i = 1:3
-        disp("The sampled torque for Joint " + i + " is " + sampled_torque(i));
-    end
-end
-
-% ----------------------------------------------------------------------
-
+% center the figure on screen and resize it
+fig_size = get(0, 'Screensize');
+fig_pos = [0,0,... %fig_size(3), fig_size(4), ...
+    0.9*fig_size(3), 0.8*fig_size(4)];
+set(f, 'Position', fig_pos);
+axis((Robot.l2 + Robot.l3) * [-1 1 -1 1 -0.5 1.5]);
+title('Stick figure plot');
+xlabel('X Axis [m]'); ylabel('Y Axis [m]'); zlabel('Z Axis [m]');
 
 % ------------------------- Test Force Sensing -------------------------
+
+% y = kx + y0
+% where y is the ADC count value, k = 178.5, x = torque (Nm), and y0 is the
+% initial offset calculated in the calibration step
+% x = (y - y0)/k
+
+y0 = [0 ; 0; 0]; % offset calculated in calc_offset
+k = 178.5;       % given scaling factor
 
 % Set the vertices that the arm will travel to in radians
 vertex1 = [ 0; 0; 0 ];
@@ -81,6 +74,10 @@ while 1
     POINT2 = 2;
     POINT3 = 3;
     
+    % Inside a switch statement
+    % Move the arm to the proper position
+    % Sample the arm and the force on the arm
+    % Plot the force vector on the arm
     switch user_input
         case POINT1
             
@@ -97,19 +94,61 @@ while 1
             sampled_torque = collect_n_samples(...
                 Collect_PositionandTorque_Only,num_samples,...
                 STATUS_ID,pp, statuspacket);
+            % calculate torque in Nm
+            torque_Nm = (sampled_torque - y0)/k;
             
             disp('Go to point 1');
+            disp('torque: ' + torque_Nm);
+            
+            % plot the force vector
         case POINT2
+            
+            % Send the arm to Vertex 2
+            send_point(PID_ID,pp,pidpacket,vertex2);
+            
+            % Determine what is to be sampled
+            Collect_PositionandTorque_Only = 9;    
+            num_samples = 1;            % The number of samples to take
+            statuspacket(16) = 1;       % Set the status packet to tell the
+                                        %  nucleo to sample torque
+                                        
+            % Sample the the arm to read the position and the torque sensors
+            sampled_torque = collect_n_samples(...
+                Collect_PositionandTorque_Only,num_samples,...
+                STATUS_ID,pp, statuspacket);
+            % calculate torque in Nm
+            torque_Nm = (sampled_torque - y0)/k;
+            
             disp('Go to point 2');
+            disp('torque: ' + torque_Nm);
+            
+            % plot the force vector
         case POINT3
+            
+            % Send the arm to Vertex 3
+            send_point(PID_ID,pp,pidpacket,vertex3);
+            
+            % Determine what is to be sampled
+            Collect_PositionandTorque_Only = 9;    
+            num_samples = 1;            % The number of samples to take
+            statuspacket(16) = 1;       % Set the status packet to tell the
+                                        %  nucleo to sample torque
+                                        
+            % Sample the the arm to read the position and the torque sensors
+            sampled_torque = collect_n_samples(...
+                Collect_PositionandTorque_Only,num_samples,...
+                STATUS_ID,pp, statuspacket);
+            % calculate torque in Nm
+            torque_Nm = (sampled_torque - y0)/k;
+            
             disp('Go to point 3');
+            disp('torque: ' + torque_Nm);
+            
+            % plot force vector on the end effector
         otherwise
             break
     end
-    % Inside a switch statement
-    % Move the arm to the proper position
-    % Sample the arm and the force on the arm
-    % Plot the force vector on the arm
+
 end
 
 
