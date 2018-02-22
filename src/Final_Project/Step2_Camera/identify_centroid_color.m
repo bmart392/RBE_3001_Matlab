6c9e1032f044e9794bc08142a7450201add4b974
Benjamin Mart <bmart392@gmail.com>
2018-02-22 11:35:37 -0500

    Successfully got color and centorid solution working

--- src/Final_Project/Step2_Camera/identify_centroid_color.m
+++ src/Final_Project/Step2_Camera/identify_centroid_color.m
@@ -1,4 +1,11 @@
-function [color blob_centroid] = identify_centroid_color(image)
+% This function determines which colors are present in the image and
+% returns the most populated color and the centroid fo that blob.
+% INPUT: image = a standard JPG image that has already been read in
+% OUPUT: centroid_color = a struct with two fields. The first is the color
+% which is a string and the second is a 1 x 2 array that holds the
+% centroid
+
+function  centroid_color  = identify_centroid_color(image)
 
 % To determine which function to use, determine the area of white space
 % produced by each filter and choose the color that has the most area
@@ -24,81 +31,32 @@ num_rows_green = size(green_stats,1);
 num_rows_blue = size(blue_stats,1);
 num_rows_yellow = size(yellow_stats,1);
 
-% Set the default color to choose
+% Initialize largest area to ensure that any area is accepted at first
 largest_area = 0;
 
 % Check if the most green is in the image
 if num_rows_green
     if green_stats.Area > largest_area
         largest_area = green_stats.Area;
-        color = "Green";
+        centroid_color.Color = "Green";
+        centroid_color.Centroid = green_stats.Centroid;
     end
 end
 % Check if the most  blue is in the image
 if num_rows_blue
     if blue_stats.Area > largest_area
         largest_area = blue_stats.Area;
-        color = "Blue";
+        centroid_color.Color = "Blue";
+        centroid_color.Centroid = blue_stats.Centroid;
     end
 end
 % Check if the most yellow is in the image
 if num_rows_yellow
     if yellow_stats.Area > largest_area
-        largest_area = yellow_stats.Area;
-        color = "Yellow";
+        centroid_color.Color = "Yellow";
+        centroid_color.Centroid = yellow_stats.Centroid;
     end
-else
-    error('No matching color was found');
 end
 
-blob_centroid = largest_area(1).Centroid;
-%blob_centroid(2,1) = largest_area.Centroid(1,2);
-%
-% % Display the centroid of the blob.
-% disp("Centroid");
-% disp(stat) % need to do the floor() command
-%
-% % THis is the size including the porches. The actual image is 640x480.
-% % disp(size(blob1))
-%
-% % Now we need to edit the color image so that the centroid has a
-% % cross-hairs over it.
-% % The function crosshair returns a matrix of 1's and 0's to denote if the
-% % pixel should be black or not. Eventually, it will take in an image and a
-% % centroid and return the new image with the crosshair on it.
-%
-% % This line below simply reminds us what the image in matlab is named.
-% % imshow(image)
-%
-% % Here we create the new image with the given centroid.
-% centroid = cat(1, stat.Centroid);
-% centrow = centroid(1,1);
-% centcolumn = centroid(1,2);
-%
-% centrow = floor(centrow);
-% centcolumn = floor(centcolumn);
-%
-% % Create our crosshair matrix.
-% overlaycross = crosshair(size(image), centrow, centcolumn)';
-%
-%
-% newcrossimage = image; % zeros(size(image,1), size(image, 2), 3);
-% % size(image,2)
-%
-% pause(0.25);
-%
-% % We will overlay the crosshair over the old image file.
-% for L=1:826 % row controller 826 if 2, 571 if 1
-%    for Y=1:571 % Column controller
-% %        disp(image(L,Y,:))
-%         if(overlaycross(L, Y) == 1)
-%             newcrossimage(Y,L, 1:3) = [255 255 255];
-%         else
-%             newcrossimage(Y,L, 1:3) = image(Y,L, 1:3);
-%         end
-%    end
-% end
-%
-% imshow(newcrossimage);
-
 end
+
