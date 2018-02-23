ba95a11779acf545250e62cab7478f89f4441dc7
embarinelli <embarinelli@wpi.edu>
2018-02-23 15:26:16 -0500

    Added a progress report

---
+++ src/Final_Project/Progress_Report.m
@@ -0,0 +1,8 @@
+% Inverse Kinematics still has issues but hteyre okay for the most part,
+% just leave it alone unless there are serious problems.
+
+% There needs to be an offset in the position sent to the arm becuase it is
+% usually about 1 cm off
+
+% The arm always samples itself being at the home position for some reason,
+% you should fix that.
